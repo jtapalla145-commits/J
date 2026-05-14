fba982502f6722f27fb8f3248db8e3ecc75b887f
jtapalla145-commits <jtapalla145@gmail.com>
2026-05-14 20:07:35 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+function showMessage() {
+    document.getElementById("message-contact").innerText = "Thank you for choosing Michaela Signature Sweets! Please contact us at @michaela.signaturesweets on Instagram to place your order.";
+}
